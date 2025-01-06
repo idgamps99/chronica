806e77f76d5be64c5d2ac93fa38140a5d492fc11
Myles Amphlett <mylesamphlett@gmail.com>
2025-01-06 16:00:37 +0000

    form for new diary added

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,2 +1,23 @@
 class DiariesController < ApplicationController
+
+  def new
+    @diary = Diary.new
+  end
+
+  def create
+    params = diary_params
+    params[:private] == 0 ? private = false : private = true
+    @diary = Diary.new(title: params[:title], private: private, user_id: current_user.id)
+    if @diary.save
+      redirect_to root_path
+    else
+      render "diaries/new", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:title, :private)
+  end
 end
