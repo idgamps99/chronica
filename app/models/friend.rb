7376286ec29ec83cb6018bbb1b314d8aae008a6c
Myles Amphlett <mylesamphlett@gmail.com>
2025-01-23 12:21:11 +0000

    associations added for friendships

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,4 @@
 class Friend < ApplicationRecord
-  belongs_to :user, class_name => 'User'
-  belongs_to :followed_user, class_name => 'User'
+  belongs_to :sender, class_name: "User"
+  belongs_to :recipient, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :diaries
-  has_many :friends
-  has_many :users, class_name: :Friend, foreign_key: :user_id
-  has_many :followed_users, class_name: :Friend, foreign_key: :followed_user_id
+  has_many :sent_friend_requests, class_name: 'Friend', foreign_key: :sender_id
+  has_many :received_friend_requests, class_name: 'Friend', foreign_key: :recipient_id
+  has_many :friendships, through: :sent_friend_requests, source: :recipient
+  has_many :friendships, through: :received_friend_requests, source: :sender, conditions: { friends: { accepted: true } }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def pending_friend_requests
+  received_friend_requests.where(accepted: false)
+  end
 end
