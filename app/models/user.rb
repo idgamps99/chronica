55c102709d81d8997770c18df4635a738eb4dc70
Myles Amphlett <mylesamphlett@gmail.com>
2025-01-23 13:00:41 +0000

    spoke too soon re associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,20 @@ class User < ApplicationRecord
   has_many :diaries
   has_many :sent_friend_requests, class_name: 'Friend', foreign_key: :sender_id
   has_many :received_friend_requests, class_name: 'Friend', foreign_key: :recipient_id
-  has_many :friendships, through: :sent_friend_requests, source: :recipient
-  has_many :friendships, through: :received_friend_requests, source: :sender, conditions: { friends: { accepted: true } }
+
+  # has_many :friendships, through: :sent_friend_requests, source: :recipient
+  # has_many :friendships, through: :received_friend_requests, source: :sender do
+  #   where(friends: { accepted: true })
+  # end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def pending_friend_requests
-  received_friend_requests.where(accepted: false)
+    received_friend_requests.where(accepted: false)
+  end
+
+  def friendships
+    
   end
 end
