43a8e3c2cf26f6cef0922879e73bb58b89c83fb2
Myles Amphlett <mylesamphlett@gmail.com>
2025-01-28 18:27:27 +0000

    index page for viewing friends - current and pending

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,10 @@
 class FriendshipsController < ApplicationController
   def index
     @accepted = current_user.friends
+    sender_ids = current_user.pending_received_requests.pluck(:sender_id)
+    @pending = []
+    sender_ids.each do |id|
+      @pending << User.find(id)
+    end
   end
 end
--- db/migrate/20250128175929_add_name_to_users_table.rb
+++ db/migrate/20250128175929_add_name_to_users_table.rb
@@ -1,6 +1,6 @@
 class AddNameToUsersTable < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :forename, :string, null: false
-    add_column :users, :surname, :string, null: false
+    add_column :users, :forename, :string
+    add_column :users, :surname, :string
   end
 end
